847c07298d98a9844ec8b401f93a34739201cfaf
mfaci2000 <mfaci2000@yahoo.com>
2017-11-29 11:27:24 -0600

    Add empty safari test case

---
+++ StartingWithSelenium_370/GoogleSearchEngineUsingSafari.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StartingWithSelenium_370
+{
+    [TestClass]
+    public class GoogleSearchEngineUsingSafari
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
